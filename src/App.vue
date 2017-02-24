fb13e5ccb304addbe5fb8067e387d873ada8ced6
Alix <alix.fumoleau@gmail.com>
2017-02-24 09:33:35 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1></h1>
+    <user-list></user-list>
+  </div>
+</template>
+
+<script>
+import UserList from './UserList.vue';
+import io from 'socket.io-client';
+
+export default {
+  name: 'app',
+  components: {'user-list': UserList},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created() {
+    var socketio = io('http://localhost:24602');
+    socketio.on('turn', (data) => {
+      console.log(data);
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="user-list">
+    <div v-for="user in userList">{{user.name}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-list',
+  data () {
+    return {
+      userList: []
+    }
+  },
+  created() {
+    console.log('test');
+    fetch('http://localhost:24601/users')
+    .then((res) => res.json())
+    .then((json) => { console.log(json); return json; })
+    .then((json) => { this.userList = json; });
+  }
+}
+</script>
+
+<style>
+</style>
